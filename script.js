00f3cf5c9f2499a5fc6cf04c386f5d1fff1fb4a2
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2021-10-14 22:07:23 -0400

    fix add/removing rows and cols

--- script.js
+++ script.js
@@ -4,46 +4,68 @@ let colorSelected;
 
 //Add a row
 function addR() {
-    // alert("Clicked Add Row")
     let grid = document.getElementById("grid");
     // Create a new tr element.
     let tr  = document.createElement('tr');
-    // Create a new td element.
-    let td = document.createElement('td');
-    // Append row to show up in a column
-    tr.appendChild(td);
+    
+    // if no grid on screen
+    if(numRows=== 0 && numCols === 0){
+      let td = document.createElement('td');
+      // Append row to show up in a column
+      tr.appendChild(td);
+      grid.appendChild(tr);
+      numRows=1;
+      numCols=1;
+    }
+    else{
+        for(let i=0; i < numCols; i++){
+            // Create a new td element.
+            let td = document.createElement('td');
+            // Append row to show up in a column
+            tr.appendChild(td);
+        }
     //  Append the tr to the grid
-    grid.appendChild(tr); 
+    grid.appendChild(tr);
     numRows++;
+    }
 }
 //Add a column
 function addC() {
-    // alert("Clicked Add Col")
-    numCols++;
-    // Append row to show up in a column
-    for(let i=0;i< numRows; i++){
-        console.log(document.querySelectorAll("tr")[i]);
-        console.log(i);
-        document.querySelectorAll('tr')[i].appendChild(document.createElement('td'));
+    let grid = document.getElementById("grid");
+    // get tr children
+    let rows = grid.children; 
+    for(let i=0; i < rows.length;i++) // for all tr elements, create new td element
+    {
+        let myCell = document.createElement('td'); 
+        rows[i].appendChild(myCell);
     }
+    numCols++;
 }
-
 //Remove a row
 function removeR() {
-    // alert("Clicked Remove Row")
     let grid = document.getElementById("grid");
+    // get the last tr child
     let lastRow = grid.lastElementChild;
     grid.removeChild(lastRow);
+    numRows--;
+    // reset so col and rows are equal
+    if(numRows === 0){
+        numCols = 0;
+    }
 }
 //Remove a column
 function removeC() {
-    // alert("Clicked Remove Col")
+    // for each tr, get last td child and remove
     for(let i=0; i< numRows; i++){
         let row = document.querySelectorAll('tr')[i];
-        let lastRow = row.lastElementChild;
+        let lastRow = row.lastChild;
         row.removeChild(lastRow);
     }
-
+    numCols--;
+    // reset so col and rows are equal
+    if(numCols ===0){
+        numRows = 0;
+    }
 }
 //sets global var for selected color
 function selected(){
